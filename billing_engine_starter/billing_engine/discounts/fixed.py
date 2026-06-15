5096c1526b0f507750e94e22050646b35de82b58
Anushka Das <anushkadas231@gmail.com>
2026-06-15 17:52:42 +0530

    Implement FixedAmountDiscount initialization and apply method

--- billing_engine_starter/billing_engine/discounts/fixed.py
+++ billing_engine_starter/billing_engine/discounts/fixed.py
@@ -12,8 +12,9 @@ from billing_engine.discounts.base import Discount, DiscountContext
 class FixedAmountDiscount(Discount):
     def __init__(self, amount: Money) -> None:
         # TODO Day 1
-        raise NotImplementedError("Day 1: implement FixedAmountDiscount.__init__")
-
+        self.amount = amount
     def apply(self, subtotal: Money, context: DiscountContext) -> Money:
         # TODO Day 1
-        raise NotImplementedError("Day 1: implement FixedAmountDiscount.apply")
+        if self.amount.amount > subtotal.amount:
+            return subtotal
+        return self.amount
